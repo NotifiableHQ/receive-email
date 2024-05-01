110e32fbb546ae97cebabc5769effc13524b3278
Prince John Santillan <mail@princejohnsantillan.net>
2024-05-01 23:35:14 +0800

    add read_at column

--- database/migrations/notifiable_create_received_emails_table.php
+++ database/migrations/notifiable_create_received_emails_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('received_emails', function (Blueprint $table) {
             $table->id();
             $table->string('ulid');
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
